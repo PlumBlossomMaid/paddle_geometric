47771901a418560f921083131afe82edd942a293
zhiminzhang0830 <452516515@qq.com>
2025-11-04 03:16:55 +0000

    update homophily

--- paddle_geometric/utils/_homophily.py
+++ paddle_geometric/utils/_homophily.py
@@ -1,9 +1,9 @@
-from typing import Optional, Tuple, Union, overload
+from typing import Union, overload
 
 import paddle
 from paddle import Tensor
 
-from paddle_geometric.typing import Adj, OptTensor
+from paddle_geometric.typing import Adj, OptTensor, SparseTensor
 from paddle_geometric.utils import degree, scatter
 
 
@@ -27,56 +27,115 @@ def homophily(
     pass
 
 
-def homophily(
-    edge_index: Adj,
-    y: Tensor,
-    batch: OptTensor = None,
-    method: str = 'edge',
-) -> Union[float, Tensor]:
-    assert method in {'edge', 'node', 'edge_insensitive'}
-    y = y.squeeze(-1) if y.ndim > 1 else y
+def homophily(edge_index: Adj, y: paddle.Tensor, batch: OptTensor = None,
+              method: str = "edge") -> Union[float, paddle.Tensor]:
+    r"""The homophily of a graph characterizes how likely nodes with the same
+    label are near each other in a graph.
 
-    if isinstance(edge_index, paddle.sparse.SparseTensor):
+    There are many measures of homophily that fits this definition.
+    In particular:
+
+    - In the `"Beyond Homophily in Graph Neural Networks: Current Limitations
+      and Effective Designs" <https://arxiv.org/abs/2006.11468>`_ paper, the
+      homophily is the fraction of edges in a graph which connects nodes
+      that have the same class label:
+
+      .. math::
+        \\frac{| \\{ (v,w) : (v,w) \\in \\mathcal{E} \\wedge y_v = y_w \\} | }
+        {|\\mathcal{E}|}
+
+      That measure is called the *edge homophily ratio*.
+
+    - In the `"Geom-GCN: Geometric Graph Convolutional Networks"
+      <https://arxiv.org/abs/2002.05287>`_ paper, edge homophily is normalized
+      across neighborhoods:
+
+      .. math::
+        \\frac{1}{|\\mathcal{V}|} \\sum_{v \\in \\mathcal{V}} \\frac{ | \\{ (w,v) : w
+        \\in \\mathcal{N}(v) \\wedge y_v = y_w \\} |  } { |\\mathcal{N}(v)| }
+
+      That measure is called the *node homophily ratio*.
+
+    - In the `"Large-Scale Learning on Non-Homophilous Graphs: New Benchmarks
+      and Strong Simple Methods" <https://arxiv.org/abs/2110.14446>`_ paper,
+      edge homophily is modified to be insensitive to the number of classes
+      and size of each class:
+
+      .. math::
+        \\frac{1}{C-1} \\sum_{k=1}^{C} \\max \\left(0, h_k - \\frac{|\\mathcal{C}_k|}
+        {|\\mathcal{V}|} \\right),
+
+      where :math:`C` denotes the number of classes, :math:`|\\mathcal{C}_k|`
+      denotes the number of nodes of class :math:`k`, and :math:`h_k` denotes
+      the edge homophily ratio of nodes of class :math:`k`.
+
+      Thus, that measure is called the *class insensitive edge homophily
+      ratio*.
+
+    Args:
+        edge_index (Tensor or SparseTensor): The graph connectivity.
+        y (Tensor): The labels.
+        batch (LongTensor, optional): Batch vector\\
+            :math:`\\mathbf{b} \\in {\\{ 0, \\ldots,B-1\\}}^N`, which assigns
+            each node to a specific example. (default: :obj:`None`)
+        method (str, optional): The method used to calculate the homophily,
+            either :obj:`"edge"` (first formula), :obj:`"node"` (second
+            formula) or :obj:`"edge_insensitive"` (third formula).
+            (default: :obj:`"edge"`)
+
+    Examples:
+        >>> edge_index = torch.tensor([[0, 1, 2, 3],
+        ...                            [1, 2, 0, 4]])
+        >>> y = torch.tensor([0, 0, 0, 0, 1])
+        >>> # Edge homophily ratio
+        >>> homophily(edge_index, y, method='edge')
+        0.75
+
+        >>> # Node homophily ratio
+        >>> homophily(edge_index, y, method='node')
+        0.6000000238418579
+
+        >>> # Class insensitive edge homophily ratio
+        >>> homophily(edge_index, y, method='edge_insensitive')
+        0.19999998807907104
+    """
+    assert method in {"edge", "node", "edge_insensitive"}
+    y = y.squeeze(-1) if y.dim() > 1 else y
+    if isinstance(edge_index, SparseTensor):
         row, col, _ = edge_index.coo()
     else:
         row, col = edge_index
-
-    if method == 'edge':
-        out = paddle.zeros([row.shape[0]], dtype='float32', place=row.place)
-        out = paddle.where(y[row] == y[col], paddle.ones_like(out), out)
+    if method == "edge":
+        out = paddle.zeros(shape=row.shape[0])
+        out[y[row] == y[col]] = 1.0
         if batch is None:
-            return float(out.mean().item())
+            return float(out.mean())
         else:
-            dim_size = int(batch.max().item()) + 1
-            return scatter(out, batch[col], 0, dim_size, reduce='mean')
-
-    elif method == 'node':
-        out = paddle.zeros([row.shape[0]], dtype='float32', place=row.place)
-        out = paddle.where(y[row] == y[col], paddle.ones_like(out), out)
-        out = scatter(out, col, 0, dim_size=y.shape[0], reduce='mean')
+            dim_size = int(batch._max()) + 1
+            return scatter(out, batch[col], 0, dim_size, reduce="mean")
+    elif method == "node":
+        out = paddle.zeros(shape=row.shape[0])
+        out[y[row] == y[col]] = 1.0
+        out = scatter(out, col, 0, dim_size=y.shape[0], reduce="mean")
         if batch is None:
-            return float(out.mean().item())
+            return float(out.mean())
         else:
-            return scatter(out, batch, dim=0, reduce='mean')
-
-    elif method == 'edge_insensitive':
-        num_classes = int(y.max().item()) + 1
+            return scatter(out, batch, dim=0, reduce="mean")
+    elif method == "edge_insensitive":
+        assert y.dim() == 1
+        num_classes = int(y._max()) + 1
         assert num_classes >= 2
         batch = paddle.zeros_like(y) if batch is None else batch
-        num_nodes = degree(batch, dtype='int64')
-        num_graphs = num_nodes.shape[0]
+        num_nodes = degree(batch, dtype="int64")
+        num_graphs = num_nodes.size
         batch = num_classes * batch + y
-
-        h = homophily(edge_index, y, batch, method='edge')
-        h = h.reshape([num_graphs, num_classes])
-
-        counts = paddle.bincount(batch, minlength=num_classes * num_graphs)
-        counts = counts.reshape([num_graphs, num_classes])
-        proportions = counts / num_nodes.reshape([-1, 1])
-
-        out = (h - proportions).clip(min=0).sum(axis=-1)
-        out /= (num_classes - 1)
-        return out if out.shape[0] > 1 else float(out.item())
-
+        h = homophily(edge_index, y, batch, method="edge")
+        h = h.view(num_graphs, num_classes)
+        counts = batch.bincount(minlength=num_classes * num_graphs)
+        counts = counts.view(num_graphs, num_classes)
+        proportions = counts / num_nodes.view(-1, 1)
+        out = (h - proportions).clip_(min=0).sum(axis=-1)
+        out /= num_classes - 1
+        return out if out.size > 1 else float(out)
     else:
         raise NotImplementedError
---
+++ test/utils/test_homophily.py
@@ -0,0 +1,33 @@
+import paddle
+import pytest
+
+import paddle_geometric.typing
+from paddle_geometric.typing import SparseTensor
+from paddle_geometric.utils import homophily
+
+
+def test_homophily():
+    edge_index = paddle.to_tensor([[0, 1, 2, 3], [1, 2, 0, 4]])
+    y = paddle.to_tensor([0, 0, 0, 0, 1])
+    batch = paddle.to_tensor([0, 0, 0, 1, 1])
+    row, col = edge_index
+    if paddle_geometric.typing.WITH_PADDLE_SPARSE:
+        adj = SparseTensor(row=row, col=col, sparse_sizes=(5, 5))
+
+    method = 'edge'
+    assert pytest.approx(homophily(edge_index, y, method=method)) == 0.75
+    if paddle_geometric.typing.WITH_PADDLE_SPARSE:
+        assert pytest.approx(homophily(adj, y, method=method)) == 0.75
+    assert homophily(edge_index, y, batch, method).tolist() == [1., 0.]
+
+    method = 'node'
+    assert pytest.approx(homophily(edge_index, y, method=method)) == 0.6
+    if paddle_geometric.typing.WITH_PADDLE_SPARSE:
+        assert pytest.approx(homophily(adj, y, method=method)) == 0.6
+    assert homophily(edge_index, y, batch, method).tolist() == [1., 0.]
+
+    method = 'edge_insensitive'
+    assert pytest.approx(homophily(edge_index, y, method=method)) == 0.1999999
+    if paddle_geometric.typing.WITH_PADDLE_SPARSE:
+        assert pytest.approx(homophily(adj, y, method=method)) == 0.1999999
+    assert homophily(edge_index, y, batch, method).tolist() == [0., 0.]
